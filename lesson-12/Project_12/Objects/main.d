9431bb0bb5eca4ef3edb0ed680cd547f7c8e9035
Osric Holt <osricholt@gmail.com>
2025-09-05 10:38:33 -0700

    Complete Lesson-12: Examining structures and refactoring code using CMSIS (Cortex Microcontroller Software Interface Standard)

---
+++ lesson-12/Project_12/Objects/main.d
@@ -0,0 +1,5 @@
+./objects/main.o: main.c \
+  C:\Users\OSGOD\AppData\Local\Arm\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\TM4C123GH6PM.h \
+  C:\Users\OSGOD\AppData\Local\Arm\Packs\ARM\CMSIS\6.2.0\CMSIS\Core\Include\core_cm4.h \
+  C:\Users\OSGOD\AppData\Local\Arm\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\system_TM4C123.h \
+  delay.h
