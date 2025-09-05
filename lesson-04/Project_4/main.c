0c3c87884793cadc373fdffa2cac95b49e6b5b3c
Osric Holt <osricholt@gmail.com>
2025-09-05 10:07:12 -0700

    Complete lesson-04: Navigating the data sheet to enable and use the GPIO ports

---
+++ lesson-04/Project_4/main.c
@@ -0,0 +1,26 @@
+#include <stdint.h>
+#include "TM4C123.h"                    // Device header
+
+int main(){
+   *((unsigned int *)0x400FE608U) = 0X20U;
+   *((unsigned int *)0x40025400U) = 0X0EU;
+   *((unsigned int *)0x4002551CU) = 0X0EU;
+
+   while (1) {
+       *((unsigned int *)0x400253FCU) = 0X04U;
+       
+       int counter = 0;
+       while (counter < 1000000) {
+           ++counter;
+       }
+       
+       *((unsigned int *)0x400253FCU) = 0X00U;
+       
+       counter = 0;
+       while (counter < 1000000) {
+           ++counter;
+       }
+   }
+
+    return 0;
+}
